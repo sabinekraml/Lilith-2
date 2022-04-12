cc72cdc9f97ea50e063b458e63be958793eb77e0
William Vaginay <wilof3@gmail.com>
2022-04-12 17:10:23 +0200

    adding HIG-20-013 script for validation

---
+++ examples/python/CVCF_2d-HIG-20-013.py
@@ -0,0 +1,213 @@
+##################################################################
+#
+# Lilith routine example for (CV, CF)  plots
+#
+# To put in Lilith-2.X/examples/python/ folder 
+# To execute from /Lilith-2.X root folder
+#
+# Use the libraries matplotlib (plotting) and numpy (functions)
+#
+##################################################################
+
+import sys, os
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+lilith_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(lilith_dir)
+sys.path.append('../..')
+import lilith
+
+######################################################################
+# Parameters
+######################################################################
+
+print("***** reading parameters *****")
+
+# Experimental results
+exp_input = "data/latestRun2WW.list"
+
+# Lilith precision mode
+my_precision = "BEST-QCD"
+
+# Higgs mass to test
+hmass = 125.38
+
+# Output files
+if (not os.path.exists("results")):
+    os.mkdir("results")
+output = "results/CVCF-HIG-20-013_2d.out"
+outputplot = "validations/CMS/HIG-20-013/CVCF-HIG-20-013_2d.pdf"
+
+# Scan ranges
+CV_min = 0.8
+CV_max = 1.2
+CF_min = 0.4
+CF_max = 1.6
+
+# Number of grid steps in each of the two dimensions (squared grid)
+grid_subdivisions = 100
+
+######################################################################
+# * usrXMLinput: generate XML user input
+######################################################################
+
+def usrXMLinput(mass=125.38, CV=1, CF=1, precision="BEST-QCD"):
+    """generate XML input from reduced couplings CV, CF"""
+    
+    myInputTemplate = """<?xml version="1.0"?>
+
+<lilithinput>
+
+<reducedcouplings>
+  <mass>%(mass)s</mass>
+
+  <C to="tt">%(CF)s</C>
+  <C to="bb">%(CF)s</C>
+  <C to="cc">%(CF)s</C>
+  <C to="tautau">%(CF)s</C>
+  <C to="ZZ">%(CV)s</C>
+  <C to="WW">%(CV)s</C>
+
+  <extraBR>
+    <BR to="invisible">0.</BR>
+    <BR to="undetected">0.</BR>
+  </extraBR>
+
+  <precision>%(precision)s</precision>
+</reducedcouplings>
+
+</lilithinput>
+"""
+    myInput = {'mass':mass, 'CV':CV, 'CF':CF, 'precision':precision}
+        
+    return myInputTemplate%myInput
+
+######################################################################
+# Scan initialization
+######################################################################
+
+print("***** scan initialization *****")
+
+# Prepare output
+fresults = open(output, 'w')
+
+# Initialize a Lilith object
+lilithcalc = lilith.Lilith(verbose=False,timer=False)
+# Read experimental data
+lilithcalc.readexpinput(exp_input)
+
+
+######################################################################
+# Scan routine
+######################################################################
+
+m2logLmin=10000
+max=-1
+
+print("***** running scan *****")
+
+for CV in np.linspace(CV_min, CV_max, grid_subdivisions):
+    fresults.write('\n')
+    for CF in np.linspace(CF_min, CF_max, grid_subdivisions):
+        myXML_user_input = usrXMLinput(hmass, CV=CV, CF=CF, precision=my_precision)
+        lilithcalc.computelikelihood(userinput=myXML_user_input)
+        m2logL = lilithcalc.l
+        if m2logL < m2logLmin:
+            m2logLmin = m2logL
+            CVmin = CV
+            CFmin = CF
+        fresults.write('%.5f    '%CV +'%.5f    '%CF + '%.5f     '%m2logL + '\n')
+
+fresults.close()
+
+print("***** scan finalized *****")
+print("minimum at CV, CF, -2logL_min = ", CVmin, CFmin, m2logLmin)
+
+######################################################################
+# Plot routine
+######################################################################
+
+
+print("***** plotting *****")
+
+# Preparing plot
+matplotlib.rcParams['xtick.major.pad'] = 8
+matplotlib.rcParams['ytick.major.pad'] = 8
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.yaxis.set_ticks_position('both')
+ax.yaxis.set_label_position('left')
+
+ax.xaxis.set_ticks_position('both')
+ax.xaxis.set_label_position('bottom')
+
+plt.minorticks_on()
+plt.tick_params(direction='in', labelsize=15, length=12, width=1.8)
+plt.tick_params(which='minor', direction='in', length=6, width=1)
+
+# Getting the data
+data = np.genfromtxt(output)
+
+x = data[:,0]
+y = data[:,1]
+z = data[:,2]
+
+# Substracting the -2LogL minimum to form Delta(-2LogL)
+z2=[]
+for z_el in z:
+  z2.append(z_el-z.min())
+
+# Interpolating the grid
+xi = np.linspace(x.min(), x.max(), grid_subdivisions)
+yi = np.linspace(y.min(), y.max(), grid_subdivisions)
+
+X, Y = np.meshgrid(xi, yi)
+Z = griddata((x, y), z2, (X, Y), method="linear")
+
+CS = plt.contourf(xi,yi,Z,[10**(-10),2.3,5.99],colors=['#ff3300','#ffa500'], \
+              vmin=0, vmax=20, origin='lower', extent=[x.min(), x.max(), y.min(), y.max()])
+
+
+ax.set_aspect((CV_max-CV_min)/(CF_max-CF_min))
+
+# read data for official 68% and 95% CL contours & plot (added by TQL)
+expdata = np.genfromtxt('validations/CMS/HIG-20-013/CMS-HIG-20-013_CVCF-Grid.txt')
+xExp68 = expdata[1:87,0]
+yExp68 = expdata[1:87,1]
+plt.plot(xExp68,yExp68,'--',markersize=3, color = '#ff0800', label="CMS official 68% CL")
+xExp95 = expdata[88:,0]
+yExp95 = expdata[88:,1]
+plt.plot(xExp95,yExp95,'--',markersize=3, color = '#ff7b00', label="CMS official 95% CL")
+xExpbf = expdata[0,0]
+yExpbf = expdata[0,1]
+plt.plot(xExpbf,yExpbf,'D',markersize=4, color = '#6cc7e3', label="CMS official best fit")
+plt.legend(loc='upper left')
+
+# best fit point
+plt.plot([CVmin],[CFmin], '*', markersize=8, color = 'black', label = 'Lilith best fit')
+
+# Standard Model 
+plt.plot([1], [1], '+',markersize=8, color = '#eed8d7', label="SM prediction")
+plt.legend(loc='upper right')
+
+# Title, labels, color bar...
+plt.title("  Lilith-2.1, DB 22.x develop", fontsize=14, ha="left")
+plt.xlabel(r'$C_V$',fontsize=20)
+plt.ylabel(r'$C_F$',fontsize=20)
+plt.text(0.82, 0.52, r'Data from CMS-HIG-20-013 Fig. 22 + Fig. 23', fontsize=10.5)
+
+fig.set_tight_layout(True)
+
+#plt.show()
+
+# Saving figure (.pdf)
+fig.savefig(outputplot)
+
+print("results are stored in", lilith_dir + "/validations/CMS/HIG-19-015/")
+print("***** done *****")
+
