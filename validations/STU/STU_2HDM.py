7c4f5a38e991e32fdcb2d6076547b11fdc773a87
William Vaginay <wilof3@gmail.com>
2022-05-05 16:56:46 +0200

    STU calculation file using arXiv:1107.0975 formulae and plot 2 from arXiv:2204.03796

---
+++ validations/STU/STU_2HDM.py
@@ -0,0 +1,132 @@
+import numpy as np
+
+# Reference
+# [1]	arXiv:1107.0975
+# [2] arXiv:2204.03796
+# [3] arXiv:0902.0851
+
+#test
+mW = 80.281
+
+# Input parameters
+#mW = 80.398 # mass of W-boson in GeV
+mZ = 91.1876 # mass of Z-boson in GeV
+Gf = 1.16637*10**(-5) # Fermi constant in GeV^-2
+sW2 = 0.23116 # sin^2 theta_w
+sW = np.sqrt(sW2)
+cW2 = 1-sW2 # cos^2 theta_w
+cW = np.sqrt(cW2)
+
+# 2HDM parameters
+mh = 125. # Mass of the lightest neutral Higgs boson in GeV
+mhref = 125. #  Mass of the lightest neutral Higgs boson in GeV
+mH = 1000. # Mass of the heavier CP-even Higgs boson in GeV
+mHpm = 1000. # Mass of the charged Higgs boson in GeV
+mA = 1000. # Mass of the CP-ood Higgs boson in GeV
+cosba = 0. # cos(beta-alpha)
+sinba = np.sqrt(1.-cosba**2) # sin(beta-alpha)
+
+# Auxiliary loop functions
+
+#Eqs. from footnot on page 22 in [1]
+def F(x,y):
+	if x-y == 0:
+		return 0
+	else :
+		return (x+y)/2 - (x*y)*np.log(x/y)/(x-y)
+
+def f(x1,x2):
+	if delta(x1,x2) > 0 :
+		return -2*np.sqrt(delta(x1,x2))*( 
+		np.arctan( (x1-x2+1)/np.sqrt(delta(x1,x2)) ) - np.arctan( (x1-x2-1)/np.sqrt(delta(x1,x2)) ) )
+	elif delta(x1,x2) == 0 :
+		return 0
+	elif delta(x1,x2) < 0 :
+		return np.sqrt(-delta(x1,x2))*np.log( abs( (X(x1,x2)+np.sqrt(-delta(x1,x2)))/(X(x1,x2)-np.sqrt(-delta(x1,x2))) ) )
+
+def X(x1,x2):
+	return x1+x2-1
+
+def delta(x1,x2):
+	return 2*(x1+x2) - (x1-x2)**2 - 1
+
+def G(x):
+	return -4*np.sqrt(4*x-1)*np.arctan( 1/np.sqrt(4*x-1) )
+
+#Eqs. from footnot 19 on page 25 in [1]
+def B22(q,m1,m2):
+	x1 = m1/q
+	x2 = m2/q
+	if m1 == m2 :
+		return (q/24)*(
+		2*np.log(q) + 2*np.log(x1) + (16*x1 - 10/3) + (4*x1 - 1)*G(x1) )
+	else :
+		return (q/24)*(
+		2*np.log(q) + np.log(x1*x2)
+		+ ( (x1-x2)**3 -3*(x1**2-x2**2) + 3*(x1-x2) )*np.log(x1/x2)
+		- ( 2*(x1-x2)**2 - 8*(x1+x2) + 10/3 )
+		- ( (x1-x2)**2 - 2*(x1 + x2) + 1 )*f(x1,x2) 
+		- 6*F(x1,x2) )
+
+def B0(q,m1,m2):
+	x1 = m1/q
+	x2 = m2/q
+	if m1 == m2 :
+		return 2 - 2*np.sqrt(4*x1-1)*np.arctan( 1/np.sqrt(4*x1-1) )
+	else : 
+		return 1 + (1/2)*( (x1+x2)/(x1-x2) - (x1-x2) )*np.log(x1*x2) + (1/2)*f(x1,x2)
+
+def B0barre(m1, m2, m3):
+	return ( m1*np.log(m1) - m3*np.log(m3) )/(m1-m3) - ( m1*np.log(m1) - m2*np.log(m2) )/(m1-m2)
+
+
+#Formulae for the oblique parameters S, T, U for the 2HDM
+
+#Eq. (21) in [1]
+def Scalc(mh, mH, mA, mHpm, sinba):
+	cosba = np.sqrt(1-sinba**2) #cos(beta-alpha)
+	cste = 1/(np.pi*mZ**2)
+	return cste*(
+	sinba**2*B22(mZ**2,mH**2,mA**2) - B22(mZ**2, mHpm**2, mHpm**2)
+	+ cosba**2*( B22(mZ**2,mh**2,mA**2) + B22(mZ**2,mZ**2,mH**2) - B22(mZ**2,mZ**2,mh**2) - mZ**2*B0(mZ**2,mZ**2,mH**2) + mZ**2*B0(mZ**2,mZ**2,mh**2) ) )
+
+#def Scalcalign(mh, mH, mA, mHpm, sinba):
+#	cosba = np.sqrt(1-sinba**2) cos(beta-alpha)
+#	cste = 1/(np.pi*mZ**2)
+#	return cste*(
+#	B22(mZ**2,mH**2,mA**2) - B22(mZ**2, mHpm**2, mHpm**2) )
+
+#Eq. (22) in [1]
+def Tcalc(mh, mH, mA, mHpm, sinba):
+	cosba = np.sqrt(1-sinba**2) #cos(beta-alpha)
+	cste = 1/(16*np.pi*mW**2*sW2)
+	return cste*( 
+	F(mHpm**2,mA**2) + sinba**2*(F(mHpm**2,mH**2)  - F(mA**2,mH**2) )
+	+ cosba**2*( F(mHpm**2,mh**2) - F(mA**2,mh**2) + F(mW**2,mH**2) - F(mW**2,mh**2) - F(mZ**2,mH**2) + F(mZ**2,mh**2) + 4*mZ**2*B0barre(mZ**2,mH**2,mh**2) - 4*mW**2*B0barre(mW**2,mH**2,mh**2) ) ) 
+
+#def Tcalcalign(mh, mH, mA, mHpm, sinba):
+#	cosba = np.sqrt(1-sinba**2) #cos(beta-alpha)
+#	cste = 1/(16*np.pi*mW**2*sW2)
+#	return cste*( 
+#	F(mHpm**2,mH**2) + F(mHpm**2,mA**2) - F(mH**2,mA**2) )
+
+#Eq. (23) in [1]
+def Ucalc(mh, mH, mA, mHpm, sinba):
+	cosba = np.sqrt(1-sinba**2) #cos(beta-alpha)
+	cste = 1/(np.pi*mZ**2)
+	return - Scalc(mh, mH, mA, mHpm, sinba) + cste*(
+	B22(mW**2,mA**2,mHpm**2) - 2*B22(mW**2,mHpm**2,mHpm**2) 
+	+ sinba**2*B22(mW**2,mH**2,mHpm**2) 
+	+ cosba**2*( B22(mW**2,mh**2,mHpm**2) + B22(mW**2,mW**2,mH**2) - B22(mW**2,mW**2,mh**2) - mW**2*B0(mW**2,mW**2,mH**2) + mW**2*B0(mW**2,mW**2,mh**2) ) ) 
+
+
+#Numerical Checks
+
+#print("S with values from Fig. 2 of [2] = ", Scalc(mh = 125, mH = 1396, mA = 1024, mHpm = 1000, sinba = 1))
+#print("T with values from Fig. 2 of [2] = ", Tcalc(mh = 125, mH = 1396, mA = 1024, mHpm = 1000, sinba = 1))
+#print("U with values from Fig. 2 of [2] = ", Ucalc(mh = 125, mH = 1396, mA = 1024, mHpm = 1000, sinba = 1))
+
+#print("S with values from App. A in [3] = ", Scalc(mh = 80, mH = 200, mA = 140, mHpm = 160, sinba = 0.2))
+#print("T with values from App. A in [3] = ", Tcalc(mh = 80, mH = 200, mA = 140, mHpm = 160, sinba = 0.2))
+#print("U with values from App. A in [3] = ", Ucalc(mh = 80, mH = 200, mA = 140, mHpm = 160, sinba = 0.2))
+
---
+++ validations/STU/mHmA_ST.py
@@ -0,0 +1,177 @@
+##################################################################
+#
+# (S,T) with fixed U = 0, using best fit and correlations from Table 3 :
+# https://arxiv.org/pdf/2204.03796.pdf
+#
+##################################################################
+
+import sys, os
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import STU_2HDM as calc
+
+lilith_dir = "/home/Willy/Lilith/Lilith-2/"
+sys.path.append(lilith_dir)
+import lilith
+
+validation_dir = lilith_dir+"validations/STU/"
+
+print("lilith_dir: ",lilith_dir)
+print("validation_dir: ",validation_dir)
+
+######################################################################
+# Parameters
+######################################################################
+
+print("***** reading parameters *****")
+
+# Values
+Scen = 0.15
+Ssigma = 0.08
+Tcen = 0.27
+Tsigma = 0.06
+STcorrelation = 0.93
+
+# Output files
+output = validation_dir+"mHmA_ST.out"
+outputplot = validation_dir+"mHmA_ST.pdf"
+
+# Scan ranges
+mH_min = -400
+mH_max = 400
+mA_min = -400
+mA_max = 400
+
+# Number of grid steps in each of the two dimensions (squared grid)
+grid_subdivisions = 100
+
+######################################################################
+# Scan initialization
+######################################################################
+
+print("***** scan initialization *****")
+
+# Prepare output
+fresults = open(output, 'w')
+
+######################################################################
+# Likelihood Calculation
+######################################################################
+
+def func(X, cen1, cen2, sig1p, sig1m, sig2p, sig2m, p):
+    z1, z2 = X[0], X[1]
+    z10, z20 = cen1, cen2
+
+    V1 = sig1p * sig1m
+    V1e = sig1p - sig1m
+    V2 = sig2p * sig2m
+    V2e = sig2p - sig2m
+    V1f = V1 + V1e * (z1 - z10)
+    V2f = V2 + V2e * (z2 - z20)
+    L2t = 1 / (1 - p ** 2) * ( (z1 - z10) ** 2 / V1f - 2 * p * (z1 - z10) * (z2 - z20) / np.sqrt(V1f * V2f) + (z2 - z20) ** 2 / V2f )
+    return L2t
+
+######################################################################
+# Scan routine
+######################################################################
+
+m2logLmin=10000
+max=-1
+
+print("***** running scan *****")
+
+for mH in np.linspace(mH_min, mH_max, grid_subdivisions):
+    fresults.write('\n')
+    for mA in np.linspace(mA_min, mA_max, grid_subdivisions):
+        m2logL = func([calc.Scalc(mh = 125, mH = mH + 1000, mA = mA + 1000, mHpm = 1000, sinba = 1) , calc.Tcalc(mh = 125, mH = mH + 1000, mA = mA + 1000, mHpm = 1000, sinba = 1) ], Scen, Tcen, Ssigma, Ssigma, Tsigma, Tsigma, STcorrelation)
+#mH = mH + 1000 and same for mA cause it's delta mH actually so mH-mHpm, with here mHpm = 1000
+        if m2logL < m2logLmin:
+            m2logLmin = m2logL
+            mHmin = mH
+            mAmin = mA
+        fresults.write('%.5f    '%mH +'%.5f    '%mA + '%.5f     '%m2logL + '\n')
+
+fresults.close()
+
+print("***** scan finalized *****")
+print("minimum at mH, mA, -2logL_min = ", mHmin, mAmin, m2logLmin)
+
+
+######################################################################
+# Plot routine
+######################################################################
+
+print("***** plotting *****")
+
+# Preparing plot
+matplotlib.rcParams['xtick.major.pad'] = 8
+matplotlib.rcParams['ytick.major.pad'] = 8
+
+fig = plt.figure( figsize=(5,5) )
+ax = fig.add_subplot(111)
+
+ax.yaxis.set_ticks_position('both')
+ax.yaxis.set_label_position('left')
+
+ax.xaxis.set_ticks_position('both')
+ax.xaxis.set_label_position('bottom')
+
+plt.minorticks_on()
+plt.tick_params(direction='in', labelsize=14, length=10, width=2)
+plt.tick_params(which='minor', direction='in', length=7, width=1.2)
+
+
+# Getting the data
+data = np.genfromtxt(output)
+
+x = data[:,0]
+y = data[:,1]
+z = data[:,2]
+
+
+# Substracting the -2LogL minimum to form Delta(-2LogL)
+z2=[]
+for z_el in z:
+  z2.append(z_el-z.min())
+
+
+# Interpolating the grid
+xi = np.linspace(x.min(), x.max(), grid_subdivisions)
+yi = np.linspace(y.min(), y.max(), grid_subdivisions)
+
+X, Y = np.meshgrid(xi, yi)
+Z = griddata((x, y), z2, (X, Y), method="linear")
+
+
+# Plotting the 68% and 95% CL regions
+ax.contourf(xi,yi,Z,[10**(-10),2.3,5.99],colors=['#ff3300','#ffa500'])#, \
+              #vmin=0, vmax=20, origin='lower', extent=[x.min(), x.max(), y.min(), y.max()])
+
+ax.set_aspect((mH_max-mH_min)/(mA_max-mA_min))
+
+# best fit point
+plt.plot([mHmin],[mAmin], '+', markersize=8, color = 'black', label = 'best fit')
+plt.legend(loc='upper right')
+
+# Title, labels, color bar...
+#plt.title("Lilith-2.1, DB 22.x validation", fontsize=12, ha="center")
+plt.xlabel(r'$\Delta m_H$[GeV]',fontsize=16)
+plt.ylabel(r'$\Delta m_A$[GeV]',fontsize=16)
+plt.text(-360, 335, r'$\cos(\beta - \alpha) = 0$, $m_{H^{\pm}} = 1TeV$', fontsize=9)
+plt.text(-360, 285, f"Best points ($\Delta m_H, \Delta m_A$) = ({mHmin:.0f}, {mAmin:.0f})", fontsize=9)
+plt.text(-360, 255, f"with $\chi^{2}$ = {m2logLmin:.3f}, S = {calc.Scalc(mh = 125, mH = mHmin + 1000, mA = mAmin + 1000, mHpm = 1000, sinba = 1):.3f}, T = {calc.Tcalc(mh = 125, mH = mHmin + 1000, mA = mAmin + 1000, mHpm = 1000, sinba = 1):.3f}", fontsize=9)
+plt.text(-360, 205, f"CDF Best points ($\Delta m_H, \Delta m_A$) = (396, 24)", fontsize=9)
+plt.text(-360, 175, f"with $\chi^{2}$ = 3.04, S = 0.01, T = 0.173", fontsize=9)
+
+fig.set_tight_layout(True)
+
+#plt.show()
+
+# Saving figure (.pdf)
+fig.savefig(outputplot)
+
+print("results are stored in", lilith_dir + "/validations/STU/")
+print("***** done *****")
+
