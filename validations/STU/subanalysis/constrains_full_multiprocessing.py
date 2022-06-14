f09b1ba93476c741b422e82325c48a11bb206fce
William Vaginay <wilof3@gmail.com>
2022-06-14 23:10:28 +0200

    multiprocessing

---
+++ validations/STU/subanalysis/constrains_full_multiprocessing.py
@@ -0,0 +1,184 @@
+import sys, os
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import matplotlib
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import subprocess
+import time
+import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor as executor 
+from multiprocessing import Pool
+
+lilith_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))+"/"
+validation_dir = lilith_dir+"validations/STU/subanalysis/"
+calc2HDM_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))+"/2HDMc/2HDMC-1.8.0/"
+
+
+######################################################################
+# Parameters
+######################################################################
+
+
+#print("m12, sba = ", np.cos( np.arctan(2.18) - np.arccos(0.25) ) * (200/np.sqrt(2.18)), np.sqrt(1-0.25**2) )
+
+# 2HDM type = 1, 2
+type = 1
+
+# Scan ranges
+mA_min = 200
+mA_max = 2000
+mH_min = 200
+mH_max = 2000
+mHpm_min = 200
+mHpm_max = 2000
+if type == 1:
+  cba_min = -0.25
+  cba_max = 0.25
+  tb_min = 0.1
+  tb_max = 10
+if type == 2:
+  cba_min = -0.05
+  cba_max = 0.05
+  tb_min = 0.1
+  tb_max = 10
+
+# Precisions
+mH_precision = 50
+mA_precision = 50
+mHpm_precision = 50
+cba_precision = 20
+tb_precision = 20
+#mH_precision = 2
+#mA_precision = 5
+#mHpm_precision = 5
+#cba_precision = 5
+#tb_precision = 5
+
+# Lists
+mHlist = []
+for mH in np.linspace(mH_min, mH_max, mH_precision):
+	mHlist.append(mH)
+
+iterationlist = []
+for i in range(mH_precision):
+	iterationlist.append(i)
+
+# Output files
+output = []
+outputplot = []
+if type == 1:
+	for i in range(mH_precision):
+		output.append(validation_dir+"multiprocessing/constrains" + "_" + str(mA_precision) + "_" + str(mH_precision) + "_" + str(mHpm_precision) + "_" + str(cba_precision) + "_" + str(tb_precision) + "_" + "I" + "_" + str(i) + ".out")
+		outputplot = validation_dir+"multiprocessing/constrains" + "_" + str(mA_precision) + "_" + str(mH_precision) + "_" + str(mHpm_precision) + "_" + str(cba_precision) + "_" + str(tb_precision) + "_" + "I" + "_" + str(i) + ".pdf"
+
+	outputfinal = validation_dir+"multiprocessing/constrains" + "_" + str(mA_precision) + "_" + str(mH_precision) + "_" + str(mHpm_precision) + "_" + str(cba_precision) + "_" + str(tb_precision) + "_" + "I" + ".out"
+
+
+if type == 2:
+	for i in range(mH_precision):
+		output = validation_dir+"multiprocessing/constrains" + "_" + str(mA_precision) + "_" + str(mH_precision) + "_" + str(mHpm_precision) + "_" + str(cba_precision) + "_" + str(tb_precision) + "_" + "II" + "_" + str(i) + ".out"
+		outputplot = validation_dir+"multiprocessing/constrains" + "_" + str(mA_precision) + "_" + str(mH_precision) + "_" + str(mHpm_precision) + "_" + str(cba_precision) + "_" + str(tb_precision) + "_" + "II" + "_" + str(i) + ".pdf"
+
+	outputfinal = validation_dir+"multiprocessing/constrains" + "_" + str(mA_precision) + "_" + str(mH_precision) + "_" + str(mHpm_precision) + "_" + str(cba_precision) + "_" + str(tb_precision) + "_" + "II" + ".out"
+
+######################################################################
+# Tests
+######################################################################
+
+
+
+######################################################################
+# Definition
+######################################################################
+
+def func(iteration):
+
+	# Prepare output
+	fresults = open(output[iteration], 'w')
+	i=1
+	mH = mHlist[iteration]
+
+	for mA in np.linspace(mA_min, mA_max, mA_precision):
+		if i==1 and iteration==0:
+				print("mA = ", mA, flush=True)
+		if i%2==0 and iteration==0:
+				print("mA = ", mA, flush=True)
+		i+=1
+		for mHpm in np.linspace(mHpm_min, mHpm_max, mHpm_precision):
+			cons = False
+			cba_cons = 0
+			tb_cons = 0
+			for cba in np.linspace(cba_min, cba_max, cba_precision):
+#				print("cba = ", cba)
+#				print("cons = ", cons)
+				for tb in np.linspace(tb_min, tb_max, tb_precision):
+					m12 = np.cos( np.arctan(tb) - np.arccos(cba) ) * (mH/np.sqrt(tb))
+					sba = np.sqrt(1-cba**2)
+					p1 = subprocess.run([calc2HDM_dir+'CalcPhys', '125.00000', str(mH), str(mA), str(mHpm), str(sba), '0.00000', '0.00000', str(m12), str(tb), str(type)], capture_output=True, text=True)
+					Treelevelunitarity, Perturbativity, Stability = int(p1.stdout[969]), int(p1.stdout[994]), int(p1.stdout[1019])
+					
+					if Treelevelunitarity == 1 and Perturbativity == 1 and Stability == 1:
+						cons = True
+						cba_cons = cba
+						tb_cons = tb
+			
+			if cons:					
+				fresults.write('%.2f    '%mH + '%.2f    '%mA + '%.2f    '%mHpm + '1    ' + '%.2f    '%cba_cons + '%.2f    '%tb_cons + '\n')
+			else:
+				fresults.write('%.2f    '%mH + '%.2f    '%mA + '%.2f    '%mHpm + 'nan    ' + '%.2f    '%cba_cons + '%.2f    '%tb_cons + '\n')
+
+	fresults.close()
+
+######################################################################
+# Multiprocessing
+######################################################################
+
+start = time.perf_counter()
+
+if __name__ == '__main__':
+	pool = Pool()
+	pool.map(func, iterationlist)
+
+stop = time.perf_counter()
+
+print("time = ", stop-start, flush=True)
+
+fresultsfinal = open(outputfinal, 'w')
+for i in iterationlist:
+	fresults = open(output[i])
+	content = fresults.read()
+	fresultsfinal.write(content+"\n")
+	fresults.close()
+fresultsfinal.close()
+
+######################################################################
+# Plot routine
+######################################################################
+
+## Preparing plot
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+
+## Getting the data
+#data = np.genfromtxt(output)
+#x = data[:,0]
+#y = data[:,1]
+#z = data[:,2]
+#consvalue = data[:,3]
+
+## Plotting
+#sc = ax.scatter(x, y, z, c=consvalue)
+##cbar = fig.colorbar(sc)
+
+## Title, labels, color bar...
+#ax.set_xlabel(r'$m_H$[GeV]',fontsize=10)
+#ax.set_ylabel(r'$m_A$[GeV]',fontsize=10)
+#ax.set_zlabel(r'$m_{H^{\pm}}$[GeV]',fontsize=10)
+
+##plt.show()
+
+## Saving figure (.pdf)
+#fig.savefig(outputplot)
+
+#print("results are stored in", validation_dir, flush=True)
